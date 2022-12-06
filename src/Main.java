7cde91cdcd6d21dd02f9e835e55db4273893e5f4
Кристина <duleba@eml.ru>
2022-12-06 20:29:08 +0700

    домашнее задание Автомобиль

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,59 @@
 public class Main {
     public static void main(String[] args) {
+       /* Автомобиль Lada = new Автомобиль();
+        Lada.setBrand("Lada");
+        Lada.setModel("Granta");
+        Lada.setYear(2015);
+        Lada.setCountry("России");
+        Lada.setColor("Желтый");
+        Lada.setEngineVolume(1.7);
 
+        Автомобиль Audi= new Автомобиль();
+        Audi.setBrand("Audi");
+        Audi.setModel("A8 50 L TDI quattro");
+        Audi.setYear(2020);
+        Audi.setCountry("Германии");
+        Audi.setColor("Черный");
+        Audi.setEngineVolume(3.0);
 
+        Автомобиль BMW= new Автомобиль();
+        BMW.setBrand("BMW");
+        BMW.setModel("Z8");
+        BMW.setYear(2021);
+        BMW.setCountry("Германии");
+        BMW.setColor("Черный");
+        BMW.setEngineVolume(3.0);
+
+        Автомобиль Kia= new Автомобиль();
+        Kia.setBrand("Kia");
+        Kia.setModel("Sportage");
+        Kia.setYear(2018);
+        Kia.setCountry("Южной корее");
+        Kia.setColor("Красный");
+        Kia.setEngineVolume(2.4);
+
+        Автомобиль Hyundai = new Автомобиль();
+        Hyundai .setBrand("Hyundai ");
+        Hyundai .setModel("Avante");
+        Hyundai .setYear(2016);
+        Hyundai .setCountry("Южной корее");
+        Hyundai .setColor("Оранжевый");
+        Hyundai .setEngineVolume(1.6);*/
+
+        Автомобиль Lada = new Автомобиль("Lada", "Granta", 2015, "России", "желтый",
+                1.7);
+        Автомобиль Audi = new Автомобиль("Audi", "A8 50 L TDI quattro", 2020, "Германии", "черный",
+                3.0);
+        Автомобиль BMW = new Автомобиль("BMW", "Z8", 2021, "Германии", "", 3.0);
+        Автомобиль Kia = new Автомобиль("Kia", "Sportage", 2018, "Южной Корее", "красный",
+                2.4);
+        Автомобиль Hyundai = new Автомобиль("Hyundai", "Avante", 2016, "Южной Корее", "оранжевый",
+                1.6);
+
+        System.out.println(Lada.toString());
+        System.out.println(Audi.toString());
+        System.out.println(BMW.toString());
+        System.out.println(Kia.toString());
+        System.out.println(Hyundai.toString());
     }
 }
--- src/Автомобиль.java
+++ src/Автомобиль.java
@@ -1,3 +1,96 @@
 public class Автомобиль {
+    private String brand;
+    private String model;
+    private double engineVolume;
+    private String color;
+    private int year;
+    private String country;
 
+    public String getBrand() {
+        return this.brand;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public double getEngineVolume() {
+        return this.engineVolume;
+    }
+
+    private String getColor() {
+        return this.color;
+    }
+
+    private int getYear() {
+        return this.year;
+    }
+
+    private String getCountry() {
+        return this.country;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setEngineVolume(double engineVolume) {
+        this.engineVolume = engineVolume;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String toString() {
+        return brand + " " + model + ", " + year + "год выпуска, " + "сборка в " + country + ", " + color +
+                " цвет кузова, объем двигателя - " + engineVolume;
+    }
+
+    public Автомобиль(String brand, String model, int year, String country, String color, double engineVolume) {
+        if (brand == null || brand.equals("")) {
+            brand = "default";
+        }
+        this.brand = brand;
+
+        if (model == null || model.equals("")) {
+            model = "default";
+        }
+        this.model = model;
+
+        if (year <= 0) {
+            this.year = 2000;
+        } else {
+            this.year = year;
+        }
+
+        if (country == null || country.equals("")) {
+            country = "default";
+        }
+        this.country = country;
+
+        if (color == null || color.equals("")) {
+            color = "белый";
+        }
+        this.color = color;
+
+        if (engineVolume <= 0) {
+            this.engineVolume = 1.5;
+        } else {
+            this.engineVolume = engineVolume;
+        }
+
+    }
 }
